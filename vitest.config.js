fb4c733eb08a5a899466fc0b756de039a1a34716
Carmen Alvarez <carmen@rmen.ca>
2026-02-15 16:08:01 +0100

    ci: Setup github workflow to run tests on PRs and main branch.

--- vitest.config.js
+++ vitest.config.js
@@ -4,8 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
+    reporters: process.env.CI
+      ? ["default", "github-actions", "html"]
+      : ["default"],
     environment: "jsdom",
     globals: true,
     setupFiles: "./src/__tests__/setup.js",
+    coverage: {
+      reporter: ["json-summary", "json", "html"],
+    },
+    testTimeout: 10000,
   },
 });
